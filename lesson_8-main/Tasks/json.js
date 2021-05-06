9570bf28c72b19e35bf9802a27367f89e05fc059
Dmytro Hryhorchuck <dimagrig218@gmail.com>
2021-05-06 17:12:35 +0300

    8 lesson 1 task completed

--- lesson_8-main/Tasks/json.js
+++ lesson_8-main/Tasks/json.js
@@ -1,3 +1,37 @@
+const convertToJsonForm = document.getElementById('convertToJsonForm')
+const convertFromJsonForm = document.getElementById('convertFromJsonForm')
+const convertToJsonFormInputs = convertToJsonForm.querySelectorAll('input')
+const convertToJsonFormButton = convertToJsonForm.querySelector('button')
+const convertFromJsonFormButton = convertFromJsonForm.querySelector('button')
+const convertFromJsonFormInput = convertFromJsonForm.querySelector('input')
+
+const dataObj = {};
+
+const getJsonData = () => {
+  convertToJsonFormInputs.forEach((input)=>{
+ dataObj[`${input.name}`] = input.value
+  })
+const jsonData = JSON.stringify(dataObj);
+convertFromJsonFormInput.value = jsonData;
+console.log(jsonData)
+return jsonData
+}
+
+convertToJsonFormButton.addEventListener('click', (e)=>{
+  e.preventDefault()
+    getJsonData() 
+})
+
+const getObjFromJson = (jsonData) => {
+  console.log(JSON.parse(jsonData))
+}
+
+convertFromJsonForm.addEventListener('click', (e)=>{
+  e.preventDefault()
+  getObjFromJson(getJsonData())
+})
+
+
 
 /*
   Задание:
